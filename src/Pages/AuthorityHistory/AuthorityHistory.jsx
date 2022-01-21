f8d358ab3e893c72eaee1698ec6d5529798f5157
aRbhaSun20 <arbhasun1606@gmail.com>
2022-01-21 13:48:02 +0530

    initial submits

---
+++ src/Pages/AuthorityHistory/AuthorityHistory.jsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import NavbarAfter from "../../Components/Navbar/NavbarAfter";
+import LeftBar from "../AuthorityDashboard/LeftBar";
+import PatientDetails from "./PatientDetails";
+
+export default function AuthorityHistory() {
+  return (
+    <React.Fragment>
+      <div style={{ display: "grid", gridTemplateColumns: "auto 1fr" }}>
+        <LeftBar />
+        <div
+          style={{
+            backgroundColor: "#F4F4FC",
+            display: "grid",
+            width: "100%",
+            height: "100vh",
+            gridTemplateRows: "10vh auto",
+          }}
+        >
+          <NavbarAfter />
+          <PatientDetails />
+          <br />
+          <PatientDetails />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
--- src/Pages/Router.js
+++ src/Pages/Router.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AuthorityChat from "./AuthorityChat/AuthorityChat";
+import AuthorityDashboard from "./AuthorityDashboard/AuthorityDashboard";
+import AuthorityHistory from "./AuthorityHistory/AuthorityHistory";
+import AuthorityRequest from "./AuthorityRequest/AuthorityRequest";
 import Home from "./Home";
+import PharmacyMap from "./PharmacyMaps/PharmacyMap";
 
 export default function Router() {
   return (
@@ -8,6 +13,15 @@ export default function Router() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="authoritydashboard" element={<AuthorityDashboard />} />
+          <Route path="authorityhistory" element={<AuthorityHistory />} />
+          <Route path="authorityrequest" element={<AuthorityRequest />} />
+          <Route path="authoritychat" element={<AuthorityChat />} />
+          <Route path="patientdashboard" element={<AuthorityDashboard />} />
+          <Route path="patienthistory" element={<AuthorityHistory />} />
+          <Route path="patientrequest" element={<AuthorityRequest />} />
+          <Route path="patientchat" element={<AuthorityChat />} />
+          <Route path="pharmacyMap" element={<PharmacyMap />} />
         </Routes>
       </BrowserRouter>
     </React.Fragment>
